8c031132a0b9a7e530827ecf0e6873688834168f
Ina Grozeva <ina.grozeva13@gmail.com>
2018-02-23 11:44:46 -0500

    added mobile nav

---
+++ script/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  function openNav() {
+      document.getElementById("sideNav").style.width = "100%";
+  }
+
+  function closeNav() {
+      document.getElementById("sideNav").style.width = "0";
+    }
+
+});
