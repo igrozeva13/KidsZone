621ab47173e516065fa20c21378d72ec3aae67da
Ina Grozeva <ina.grozeva13@gmail.com>
2018-03-01 16:45:52 -0500

    added slideshow to front page

--- script/main.js
+++ script/main.js
@@ -1,10 +1,14 @@
+//Open Navigation
   function openNav() {
       document.getElementById("sideNav").style.width = "100%";
   }
 
+//Close Navigation
   function closeNav() {
       document.getElementById("sideNav").style.width = "0";
     }
+
+//Contact Us Map
 
   function initMap() {
             var uluru = {lat: 34.857480, lng: -82.334496};
@@ -19,13 +23,30 @@
           }
 
 $(document).ready(function() {
-
+  //mobile dropdown menu
   $(".dropdown-mobile").click(function() {
       $(this).next().slideToggle(500);
     })
 
+  //FAQ
   $(".question").click(function() {
         $(this).siblings().slideToggle(100);
       })
 
+
+  //Slideshow
+  var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+      var slides = document.getElementsByClassName("mySlides");
+      for (var i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1 }
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides, 4000);
+  }
 });
